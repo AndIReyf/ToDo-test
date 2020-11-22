8c5d2b46bc365e84ea192e4f86127fddf82e4d43
AndIReyf <andrey.prudius@hotmail.com>
2020-11-23 00:30:00 +0100

    add cors

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express()
 const PORT = process.env.PORT || config.get('PORT')
 
 // Middleware
+app.use(require('./middleware/cors'))
 app.use(express.json())
 
 app.use('/api/items', require('./routes/api/items'))
